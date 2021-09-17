b51436211ab099e77c9b315074bf2d55e23af57a
yohei sugigami <y.sugigami@gmail.com>
2021-09-17 18:17:45 +0900

    Update Spectre to 0.10.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "1.0.0")),
-    .package(url: "https://github.com/kylef/Spectre.git", .upToNextMinor(from: "0.10.1"))
+    .package(url: "https://github.com/kylef/Spectre.git", .upToNextMinor(from: "0.10.0"))
   ],
   targets: [
     .target(name: "URITemplate", dependencies: [], path: "Sources"),
